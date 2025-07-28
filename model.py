aa6f9282db9038ed2f2bacf1a6970625fa944f37
saberhhh <139545087+saberhhh@users.noreply.github.com>
2025-07-28 19:55:43 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,6 +8,8 @@ import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
 from torch.nn import Softmax
+from SEConv import SEConv_1 as SEConv1
+from SEConv import SEConv_2 as SEConv2
 
 
 
@@ -49,9 +51,22 @@ def get_graph_feature(x, k=20, idx=None, dim9=False):
   
     return feature      # (batch_size, 2*num_dims, num_points, k)
 
+class ReverseFusion(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ReverseFusion, self).__init__()
+        self.upsample = nn.Conv1d(in_channels, out_channels, kernel_size=1, bias=False)
+        self.attention = nn.Sequential(
+            nn.Conv1d(out_channels * 2, out_channels, kernel_size=1, bias=False),
+            nn.Sigmoid()
+        )
 
-from SEConv import SEConv_1 as SEConv1
-from SEConv import SEConv_2 as SEConv2
+    def forward(self, low_feat, high_feat):
+        high_feat = self.upsample(high_feat) 
+        fusion = torch.cat((low_feat, high_feat), dim=1)
+        attention = self.attention(fusion)
+        out = low_feat + high_feat * attention 
+        return out
+
 
 class DGCNN_semseg_s3dis(nn.Module):
     def __init__(self, args):
@@ -59,52 +74,34 @@ class DGCNN_semseg_s3dis(nn.Module):
         self.args = args
         self.k = args.k
 
-        self.bn1 = nn.BatchNorm2d(32)
-        self.bn1_1 = nn.BatchNorm2d(64)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.bn3 = nn.BatchNorm2d(32)
-        self.bn3_1 = nn.BatchNorm2d(64)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.bn5 = nn.BatchNorm2d(32)
-        self.bn5_1 = nn.BatchNorm2d(64)
-        self.bn5_2 = nn.BatchNorm2d(64)
         self.bn6 = nn.BatchNorm1d(args.emb_dims)
         self.bn7 = nn.BatchNorm1d(512)
         self.bn8 = nn.BatchNorm1d(256)
 
         self.conv1 = nn.Sequential(SEConv1(18,32),
-                                   self.bn1,
                                    nn.LeakyReLU(negative_slope=0.2))
         self.conv1_1 = nn.Sequential(SEConv2(32,64),
-                                   self.bn1_1,
                                    nn.LeakyReLU(negative_slope=0.2))
         self.conv2 = nn.Sequential(SEConv2(64,64),
-                                   self.bn2,
                                    nn.LeakyReLU(negative_slope=0.2))
-
         self.conv3 = nn.Sequential(SEConv1(18,32),
-                                   self.bn3,
                                    nn.LeakyReLU(negative_slope=0.2))
         self.conv3_1 = nn.Sequential(SEConv2(32, 64),
-                                     self.bn3_1,
                                      nn.LeakyReLU(negative_slope=0.2))
         self.conv4 = nn.Sequential(SEConv2(64,64),
-                                   self.bn4,
                                    nn.LeakyReLU(negative_slope=0.2))
 
         self.conv5 = nn.Sequential(SEConv1(18,32),
-                                   self.bn5,
                                    nn.LeakyReLU(negative_slope=0.2))
         self.conv5_1 = nn.Sequential(SEConv2(32, 64),
-                                     self.bn5_1,
                                      nn.LeakyReLU(negative_slope=0.2))
         self.conv5_2 = nn.Sequential(SEConv2(64,64),
-                                     self.bn5_2,
                                      nn.LeakyReLU(negative_slope=0.2))
-
         self.conv6 = nn.Sequential(nn.Conv1d(192, args.emb_dims, kernel_size=1, bias=False),
                                    self.bn6,
                                    nn.LeakyReLU(negative_slope=0.2))
+        self.reverse_fusion1 = ReverseFusion(64, 64)
+        self.reverse_fusion2 = ReverseFusion(64, 64)
 
         self.conv7 = nn.Sequential(nn.Conv1d(1216, 512, kernel_size=1, bias=False),
                                    self.bn7,
@@ -119,39 +116,35 @@ class DGCNN_semseg_s3dis(nn.Module):
         batch_size = x.size(0)
         num_points = x.size(2)
 
-        x1_1 = get_graph_feature(x, k=self.k)
-        x1_2 = self.conv1(x1_1)
+        x1_1 = get_graph_feature(x, k=4) 
+        x1_2 = self.conv1(x1_1)  
         x1_3 = self.conv1_1(x1_2)
-        x1_4 = self.conv2(x1_3)
-        x1 = x1_4.max(dim=-1, keepdim=False)[0]
+        x1_4 = self.conv2(x1_3) 
+        x1 = x1_4.max(dim=-1, keepdim=False)[0]  
 
-
-
-        x2_1 = get_graph_feature(x, k=6)
-        x2_2 = self.conv3(x2_1)
+        x2_1 = get_graph_feature(x, k=8)  
+        x2_2 = self.conv3(x2_1)  
         x2_3 = self.conv3_1(x2_2)
-        x2_4 = self.conv4(x2_3)
-        x2 = x2_4.max(dim=-1, keepdim=False)[0]
+        x2_4 = self.conv4(x2_3)  
+        x2 = x2_4.max(dim=-1, keepdim=False)[0]  
 
-        x3_1 = get_graph_feature(x, k=8)
+        x3_1 = get_graph_feature(x, k=12)
         x3_2 = self.conv5(x3_1)
         x3_3 = self.conv5_1(x3_2)
         x3_4 = self.conv5_2(x3_3)
         x3 = x3_4.max(dim=-1, keepdim=False)[0]
 
+        x2 = self.reverse_fusion1(x2, x3)
+        x1 = self.reverse_fusion2(x1, x2)
 
+        x = self.conv6(torch.cat((x1, x2, x3), dim=1))
+        x = x.max(dim=-1, keepdim=True)[0].repeat(1, 1, num_points)
 
-        x = torch.cat((x1, x2, x3), dim=1)
-
-        x = self.conv6(x)
-        x = x.max(dim=-1, keepdim=True)[0]
-
-        x = x.repeat(1, 1, num_points)
         x = torch.cat((x, x1, x2, x3), dim=1)
 
-        x = self.conv7(x)
-        x = self.conv8(x)
+        x = self.conv7(x) 
+        x = self.conv8(x)  
         x = self.dp1(x)
-        x = self.conv9(x)
+        x = self.conv9(x)  
 
         return x
